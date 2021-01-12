a15d71a1eef17da29703a8bc475a564e14837bc4
samuel <samsubas01@gmail.com>
2021-01-12 23:25:56 +0200

    Added component pages and navigation with react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/layout/users/Users';
 import Search from './components/layout/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/layout/pages/About';
+import User from './components/layout/users/User';
+import './App.css';
+
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    user: {},
+    loading: false,
+    alert: null
   };
   
   searchUsers = (text) => {
@@ -22,24 +29,64 @@ class App extends Component {
     this.setState({ loading: false });
   }
 
+  // Search single user
+
+  getUser = (username) => {
+    this.setState( { loading: true} );
+
+    fetch(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+
+    this.setState({ loading: false });
+  }
+
   clearUsers = () => {
     if(this.state.users.length !== 0) {
       this.setState( { users: [], loading: false } );
     }
   }
 
+  setAlert = (message, type) => {
+    this.setState( { alert: { message: message, type: type } } );
+    setTimeout(() => {
+      this.setState( { alert: null } )
+    }, 3000);
+  }
+
   render() {
 
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
 
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false}/>
-          <Users loading={loading} users={users}/>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert}/>
+
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                   <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false} setAlert={this.setAlert}/>
+                   <Users loading={loading} users={users}/>
+                </Fragment>
+              )}/> 
+
+              <Route exact path='/about' component={About}/>
+
+              <Route exact path='/user/:login' render={props => (
+                <User { ...props } getUser={this.getUser} user={user} loading={loading}/>
+              )}/>
+
+              
+              
+            </Switch>
+           
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/layout/users/UserItem.js
+++ src/components/layout/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 // Using destructuring to pull from props the user (props.user.login and so on...)
 const UserItem = ( { user: { login, avatar_url, html_url } } ) => {
@@ -9,7 +10,8 @@ const UserItem = ( { user: { login, avatar_url, html_url } } ) => {
         <img src={avatar_url} alt="" className="round-img" style={ { width: '60px' } }/>
         <h3>{login}</h3>
         <h4>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">Check Profile</a>
+          {/* <a href={ <Link to='/user'></Link> } className="btn btn-dark btn-sm my-1">Check Profile</a> */}
+          <Link to={'/user'} className="btn btn-dark btn-sm my-1">More</Link>
         </h4>
       </div>
     )
